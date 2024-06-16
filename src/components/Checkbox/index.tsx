904be3e18a2f7948527032e3a13e4393a1092158
amanShukla-amber <aman.sh@amberstudent.com>
2024-06-16 18:23:59 +0530

    initial start with checkbox

---
+++ src/components/Checkbox/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+import {CheckboxPropsType} from './types';
+
+const Checkbox = (props: CheckboxPropsType) => {
+  return (
+    <View>
+      <Text>Checkbox</Text>
+    </View>
+  );
+};
+
+export default Checkbox;
